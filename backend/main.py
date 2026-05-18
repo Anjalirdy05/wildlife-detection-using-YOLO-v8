6093d1ced29618219143017e08244357dfe8a4f6
Anjalirdy05 <anjalireddyvaka05@gmail.com>
2026-05-18 13:05:15 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://wildlife-detection-using-yolov8-rosy.vercel.app"
+    "https://wildlife-detection-using-yolov8.vercel.app"
      ],
     allow_credentials=True,
     allow_methods=["*"],
